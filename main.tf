fb2167fe70a7c8df8f9284a4ab67d9487b5d071a
jonclawson <30538332+jonclawson@users.noreply.github.com>
2024-10-30 01:09:25 -0700

    random-quiz add deployment

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "quiz-app-rg"
+  location = "westus2"
+}
+
+resource "azurerm_static_web_app" "web" {
+  name                = "random-quiz-app-jc-az-900"
+  resource_group_name = azurerm_resource_group.rg.name
+  location           = azurerm_resource_group.rg.location
+}
